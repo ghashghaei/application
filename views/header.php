c0085f3a48a09e553817b23974ab8df500b22b60
maryam <61688969+ghashghaei@users.noreply.github.com>
2020-04-01 15:42:27 +0200

    Initial commit

---
+++ views/header.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>{title}</title>
+
+	
+</head>
+<link href="<?= base_url('assets/css/bootstrap.css'); ?>" rel="stylesheet" type="text/css">
+<link href="<?= base_url('assets/css/style.css'); ?>" rel="stylesheet" type="text/css">
+<script type="application/javascript" src="<?= base_url('assets/js/js.js'); ?>"></script>
+<body>
+<div class="container">
+
